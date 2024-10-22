146d8bdd80a4a1b23534cb93ef67cc582b7a9111
Hakim-morsal2244 <m.hakim@alustudent@gmail.com>
2024-10-22 13:29:29 -0700

    Fix PEP8 issues in 8-uppercase.py and 8-main.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    """Print a string in uppercase."""
-    for char in str:
-        if ord(char) >= 97 and ord(char) <= 122:  # Check if lowercase
-            char = chr(ord(char) - 32)  # Convert to uppercase
-        print("{}".format(char), end="")
-    print()  # Print a new line at the end
+    """Print a string in uppercase followed by a new line."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:  # Check if character is lowercase
+            c = chr(ord(c) - 32)  # Convert to uppercase
+        print("{}".format(c), end="")
+    print()  # New line at the end
