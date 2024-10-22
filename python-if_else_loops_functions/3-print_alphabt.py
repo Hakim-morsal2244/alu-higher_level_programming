53e708ebd244959cf1a924dccc76b3a129d8c4aa
Hakim-morsal2244 <m.hakim@alustudent@gmail.com>
2024-10-22 11:17:08 -0700

    Fix syntax warning for character comparison

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
-        print("{}".format(chr(letter)), end="")
+
+for i in range(97, 123):
+    if chr(i) != 'e' and chr(i) != 'q':
+        print(chr(i), end="")
