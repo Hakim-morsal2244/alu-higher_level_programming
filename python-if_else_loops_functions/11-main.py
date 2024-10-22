b4c826df91c8474291f61d9a0215aafa79be9fd2
Hakim-morsal2244 <m.hakim@alustudent@gmail.com>
2024-10-22 13:52:07 -0700

    Add function to compute power of a number

---
+++ python-if_else_loops_functions/11-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+pow = __import__('11-pow').pow
+
+print(pow(2, 2))       # Output: 4
+print(pow(98, 2))      # Output: 9604
+print(pow(98, 0))      # Output: 1
+print(pow(100, -2))    # Output: 0.0001
+print(pow(-4, 5))      # Output: -1024
