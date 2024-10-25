036914d28bd689b31dfa012c24286968f6748973
Hakim-morsal2244 <m.hakim@alustudent@gmail.com>
2024-10-25 10:19:09 -0700

    task6

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -2,7 +2,6 @@
 # 5-number_keys.py
 # Brennan D Baraban <375@holbertonschool.com>
 
-
 def number_keys(a_dictionary):
     """Return the number of keys in a dictionary."""
-    return (len(a_dictionary))`
+    return len(a_dictionary)
