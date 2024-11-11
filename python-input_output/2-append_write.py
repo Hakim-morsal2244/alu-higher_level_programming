0ffe7ef6e5b414db5754710319554f8a4007868e
Hakim-morsal2244 <m.hakim@alustudent@gmail.com>
2024-11-11 06:53:38 -0800

    Task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-# File: 2-append_write.py
+"""
+This module provides a function to append text to a file and return the number
+of characters added.
+"""
+
 
 def append_write(filename="", text=""):
-    """Appends a string at the end of a text file (UTF8) and returns the number of characters added."""
+    """Appends a string at the end of a text file (UTF8) and returns the number
+    of characters added."""
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
