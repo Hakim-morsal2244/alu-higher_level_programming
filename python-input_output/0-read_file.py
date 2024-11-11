cd56b3771b15e0236787ceeb42b83cfc02dfc3e3
Hakim-morsal2244 <m.hakim@alustudent@gmail.com>
2024-11-10 23:34:12 -0800

    Task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-"""0x0B. Python - Input/Output, task 0. Read file """
-
-
 def read_file(filename=""):
-    """Reads contents of a text file and print to stdout.
-
-    Args:
-        filename (str): name of file to be opened
-
-    """
-    with open(filename, encoding='utf-8') as file:
-        print(file.read(), end='')
+    """Reads a UTF8 text file and prints it to stdout."""
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")
