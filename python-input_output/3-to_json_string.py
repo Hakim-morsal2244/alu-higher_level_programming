56587d8baba090f6af9dcd7959bfa3c5734fd4e2
Hakim-morsal2244 <m.hakim@alustudent@gmail.com>
2024-11-11 07:13:03 -0800

    Task 3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 
-
 import json
 
 
 def to_json_string(my_obj):
-    """Returns the JSON representation of an object (string)."""
-    # Convert set to list if the object is a set
-    if isinstance(my_obj, set):
-        my_obj = list(my_obj)
-    return json.dumps(my_obj)
+    """
+    Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj: The object to be serialized.
+
+    Returns:
+        A string containing the JSON representation of the object.
+    """
+    try:
+        return json.dumps(my_obj)
+    except TypeError as e:
+        raise TypeError(f"{my_obj} is not JSON serializable") from e
