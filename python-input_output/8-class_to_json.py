d208dd71024ba6523b7dc520f5e50030fa6be330
Hakim-morsal2244 <m.hakim@alustudent@gmail.com>
2024-11-13 04:56:13 -0800

    Task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,4 +7,4 @@ def class_to_json(obj):
     with simple data structure (list, dictionary, string, integer
     and boolean for JSON serialization of an object"""
 
-    return obj.__dict_
+    return obj.__dict__
