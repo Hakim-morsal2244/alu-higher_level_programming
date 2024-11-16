7fb403609b64eb79588029ef5230d6e65ff54db1
Hakim-morsal2244 <m.hakim@alustudent@gmail.com>
2024-11-16 13:59:38 -0800

    Task 12

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,3 +1,5 @@
+-- Lists all shows without a genre linked
+-- Results are sorted by ascending tv_shows.title and tv_show_genres.genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
