ff72d8d9c3efb34baa141b877faa6eaf94a8f7ea
Hakim-morsal2244 <m.hakim@alustudent@gmail.com>
2024-11-23 09:33:37 -0800

    Add unittests for max_integer function

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,37 +1,31 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
-"""
+""""max integer"""
+
+
 import unittest
+
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
-    def test_positive_numbers(self):
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+    """max integer test"""
 
-    def test_negative_numbers(self):
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
-        self.assertEqual(max_integer([-4, -3, -2, -1]), -1)
+    def test_simple_case(self):
+        """test simple cases"""
+        self.assertEqual(max_integer([1, 2, 3]), 3)
 
-    def test_mixed_numbers(self):
-        self.assertEqual(max_integer([-1, 0, 3, -4]), 3)
+    def test_negative_case(self):
+        """test negative cases"""
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
 
-    def test_single_element(self):
-        self.assertEqual(max_integer([7]), 7)
+    def test_empty_case(self):
+        """test empty cases"""
+        self.assertEqual(max_integer([]), None)
 
-    def test_empty_list(self):
-        self.assertIsNone(max_integer([]))
+    def test_max_middle_case(self):
+        """test max middle"""
+        self.assertEqual(max_integer([1, 2, 5, 3, 4]), 5)
 
-    def test_duplicates(self):
-        self.assertEqual(max_integer([2, 2, 2]), 2)
-
-    def test_none_argument(self):
-        with self.assertRaises(TypeError):
-            max_integer(None)
-
-    def test_non_integers(self):
-        with self.assertRaises(TypeError):
-            max_integer(["a", "b", "c"])
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_one_element_case(self):
+        """tests one element"""
+        self.assertEqual(max_integer([5]), 5)
