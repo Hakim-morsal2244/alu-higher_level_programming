aed844c876458b36fc6ec8abaf2183dca00bf011
Hakim-morsal2244 <m.hakim@alustudent@gmail.com>
2024-11-28 05:20:26 -0800

    a

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -66,6 +66,9 @@ class Rectangle:
         self.__y = value
 
     def area(self):
+        """
+        Calculates the area of the rectangle.
+        """
         return self.width * self.height
 
     def display(self):
