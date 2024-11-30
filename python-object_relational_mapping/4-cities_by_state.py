7c82a17cef20707890689d55aba6346463802337
Hakim-morsal2244 <m.hakim@alustudent@gmail.com>
2024-11-30 04:13:40 -0800

    Task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,47 +1,25 @@
 #!/usr/bin/python3
+"""  lists all states """
 import MySQLdb
 import sys
 
 
-def list_cities(mysql_username, mysql_password, database_name):
-    """
-    This function lists all cities from the hbtn_0e_4_usa database
-    in ascending order by cities.id.
-    """
-
-    db = MySQLdb.connect(
-        host="localhost", 
-        port=3306, 
-        user=mysql_username, 
-        passwd=mysql_password, 
-        db=database_name
-    )
-
-    cursor = db.cursor()
-
-    cursor.execute("""
-        SELECT cities.id, cities.name, states.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        ORDER BY cities.id ASC
-    """)
-
-    cities = cursor.fetchall()
-
-    for city in cities:
-        print(city)
-
-    cursor.close()
-    db.close()
-
-
-def main():
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    list_cities(mysql_username, mysql_password, database_name)
-
-
 if __name__ == "__main__":
-    main()
+    """connects"""
+    db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+            )
+    cr = db.cursor()
+    cr.execute(
+        "SELECT cities.id, cities.name, states.name FROM "
+        "cities INNER JOIN states ON states.id = cities.state_id"
+        )
+    tables = cr.fetchall()
+    for r in tables:
+        print(r)
+    cr.close()
+    db.close()
