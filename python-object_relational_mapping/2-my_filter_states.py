c64443912100cfb14ccd06342fe33a45e86bc858
Hakim-morsal2244 <m.hakim@alustudent@gmail.com>
2024-11-30 03:51:18 -0800

    Task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,36 +1,22 @@
 #!/usr/bin/python3
-"""
-This script filters and displays all states in the states table where 
-the state name matches the user input.
-
-Usage:
-    python3 2_my_filter_states.py mysql_username mysql_password 
-    database_name state_name
-"""
-import MySQLdb
+""" filters states by  input """
 import sys
-
-def filter_states():
-    # Establish a connection to the database
-    db = MySQLdb.connect(
-        host="localhost", 
-        user=sys.argv[1], 
-        passwd=sys.argv[2], 
-        db=sys.argv[3]
-    )
-    cursor = db.cursor()
-    
-    # Create and execute the query to find states by name
-    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
-    rows = cursor.fetchall()
-    
-    # Print the result
-    for row in rows:
-        print(row)
-    
-    # Close the database connection
-    cursor.close()
-    db.close()
+import MySQLdb
 
 if __name__ == "__main__":
-    filter_states()
+    """connection"""
+    with MySQLdb.connect(
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3],
+                host='localhost',
+                port=3306
+    )as engine:
+        cur = engine.cursor()
+        cur.execute(
+                "SELECT * FROM states \
+                 WHERE name LIKE BINARY'{}'".format(sys.argv[4]))
+        all_states = cur.fetchall()
+        for each in all_states:
+            print(each)
+        cur.close()
