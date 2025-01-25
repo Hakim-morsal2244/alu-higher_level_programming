88dd406e9d405c1019a8ad9fa1a84f3eacefb8cb
Hakim-morsal2244 <m.hakim@alustudent@gmail.com>
2025-01-25 00:33:23 -0800

    9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-
-// Define the function to add two integers
-const add = (a, b) => a + b;
-
-// Convert arguments to integers
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
-
-// Print the result of addition
+function add (a, b) {
+  return a + b;
+}
 console.log(add(a, b));
